3bc9735712c915c6f6a7c1577be5dcb931e5ffa6
sayyadbhasha <bhasha5a4@gmail.com>
2017-10-17 15:01:28 +0530

    1

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+/**
+ * @file Expenses Component.
+ * @author Mahesh
+ */
+import React from 'react';
+
+class Expenses extends React.Component {
+  render() {
+    return (
+      <div>Expenses</div>
+    );
+  }
+}
+
+export default Expenses;
